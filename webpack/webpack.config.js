6fe2ce7d70d8b2b9fe69cb0a1368e6d41421a675
bogdan <srk.bogdan@gmail.com>
2016-10-06 17:39:24 +0300

    webpack configuration

---
+++ webpack/webpack.config.js
@@ -0,0 +1,47 @@
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var path = require('path');
+var loaders = require('./loaders');
+
+module.exports = {
+  entry: [
+    './app/index.ts',
+    'webpack-dev-server/client?http://localhost:8080'
+  ],
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    //publicPath: '/assets/',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    root: __dirname,
+    extensions: ['', '.ts', '.js', '.json']
+  },
+  resolveLoader: {
+    modulesDirectories: ["node_modules"]
+  },
+  devtool: 'source-map',
+  plugins: [
+    //new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../app/index.html'),
+      inject: 'body',
+      hash: true
+    }),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 8080,
+      server: {
+        baseDir: path.resolve(__dirname, '../dist')
+      },
+      ui: false,
+      online: false,
+      notify: false
+    })
+  ],
+  module:{
+    loaders: loaders
+  }
+};
